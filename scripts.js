dd445eddee181566a124dcd79e3d5c239657f090
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-03-17 17:25:32 +0200

    door added

--- scripts.js
+++ scripts.js
@@ -9,15 +9,24 @@ function draw() {
  house();}
 
  function house(){
-    
+    //wall
     fill("green");
     rect(50, 250, 455, 555);
-    
+    //roof
     fill("red")
     triangle(25, 250, 530, 250, 270, 15);
-    
+    //roof window
     fill("blue")
     circle(270, 80, 50);
     line(270, 55, 270, 105);
     line(245, 80, 295, 81);
+
+    //door
+    fill("brown");
+    rect(80, 450, 80, 455);
+    //doorbell
+    fill("yellow")
+    circle(175, 500, 15);
+    fill("black")
+    circle(175, 500, 5);
   }
